9d46ffb850f1187fa6478ac212b28b40a2f8b943
dorobonneko <dorobonneko@gmail.com>
2020-02-28 15:17:29 +0800

    Initial commit

---
+++ app/src/main/java/com/moe/x4jdm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.moe.x4jdm;
+ 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity { 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+	
+} 
